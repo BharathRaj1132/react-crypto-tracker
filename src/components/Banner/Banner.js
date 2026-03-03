e39d15ced7fe7cd7aecd25d645b980b59dad86eb
manojlaptop <manojkumarsj04@gmail.com>
2026-03-03 12:12:15 +0530

    HomePage Name changed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Crypto Hunter latest
           </Typography>
           <Typography
             variant="subtitle2"
