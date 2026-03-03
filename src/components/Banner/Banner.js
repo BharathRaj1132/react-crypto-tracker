a8779248a8d1ab9af4d21aa3613161fa5fc2d4c0
manojlaptop <manojkumarsj04@gmail.com>
2026-03-03 12:21:06 +0530

    Name changes

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter latest new
+            Crypto Currency
           </Typography>
           <Typography
             variant="subtitle2"
